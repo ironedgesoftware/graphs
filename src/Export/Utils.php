2c8d9daf20ea7251427c8065b53200200bddbde3
Gustavo Falco <comfortablynumb84@gmail.com>
2016-01-09 13:00:23 -0300

    Updated to use common-utils system service instead of calling a command with "exec".

--- src/Export/Utils.php
+++ src/Export/Utils.php
@@ -10,6 +10,9 @@
 
 namespace IronEdge\Component\Graphs\Export;
 
+use IronEdge\Component\CommonUtils\Exception\CommandException;
+use IronEdge\Component\CommonUtils\System\SystemService;
+
 
 /**
  * @author Gustavo Falco <comfortablynumb84@gmail.com>
@@ -17,16 +20,62 @@ namespace IronEdge\Component\Graphs\Export;
 class Utils
 {
     /**
+     * Field _systemService.
+     *
+     * @var SystemService
+     */
+    private $_systemService;
+
+
+    /**
+     * Utils constructor.
+     *
+     * @param SystemService $systemService - System service.
+     */
+    public function __construct(SystemService $systemService = null)
+    {
+        $this->setSystemService($systemService ? $systemService : new SystemService());
+    }
+
+    /**
+     * Returns the value of field _systemService.
+     *
+     * @return SystemService
+     */
+    public function getSystemService(): SystemService
+    {
+        return $this->_systemService;
+    }
+
+    /**
+     * Sets the value of field systemService.
+     *
+     * @param SystemService $systemService - systemService.
+     *
+     * @return Utils
+     */
+    public function setSystemService(SystemService $systemService): Utils
+    {
+        $this->_systemService = $systemService;
+
+        return $this;
+    }
+
+    /**
      * Returns true if "dot" binary is installed, or false otherwise.
+     *
+     * @throws CommandException
      *
      * @return bool
      */
     public function isDotInstalled()
     {
-        exec('which dot', $output, $status);
+        try {
+            $this->getSystemService()->executeCommand('which', ['dot']);
 
-        return $status ?
-            false :
-            true;
+            return true;
+        } catch (CommandException $e) {
+            return false;
+        }
     }
 }
